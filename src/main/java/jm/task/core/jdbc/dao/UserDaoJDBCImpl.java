c1f47926bfbadb6ae0917475b78b48c7985ca7bd
levgenix <mr.lobach@gmail.com>
2020-10-06 00:34:49 +0300

    DAO implements

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,104 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+//TODO: delete IOException
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection conn;
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try {
+            conn = Util.getConnection();
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), last_name VARCHAR(255), age INT)");
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Util.close(conn);
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            conn = Util.getConnection();
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("DROP TABLE user");
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Util.close(conn);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            conn = Util.getConnection();
+            PreparedStatement pstm = conn.prepareStatement("INSERT INTO user (name, last_name, age) VALUES (?, ?, ?)");
+            pstm.setString(1, name);
+            pstm.setString(2, lastName);
+            pstm.setByte(3, age);
+            int count = pstm.executeUpdate();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Util.close(conn);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            conn = Util.getConnection();
+            PreparedStatement pstm = conn.prepareStatement("DELETE FROM user WHERE id = ?");
+            pstm.setLong(1, id);
+            int count = pstm.executeUpdate();
+//            System.out.println("Удалено " + count);
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Util.close(conn);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        try {
+            conn = Util.getConnection();
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM user"); //TODO: as Stream?
+            while(resultSet.next()) {
+                User user = new User(resultSet.getString("name"), resultSet.getString("last_name"), resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                users.add(user);
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Util.close(conn);
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try {
+            conn = Util.getConnection();
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("TRUNCATE TABLE user");
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            Util.close(conn);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,53 @@
 package jm.task.core.jdbc.util;
 
+import javax.sql.PooledConnection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Util instance;
+
+    //TODO: throws SQLException, IOException
+    public static Connection getConnection() throws SQLException, IOException {
+        if (instance == null) {
+            instance = new Util();
+        }
+
+        try {
+            Properties props = Util.getProps();
+            return DriverManager.getConnection(props.getProperty("db.url"), props.getProperty("db.username"), props.getProperty("db.password"));
+        } catch (SQLException | IOException e) {
+            throw e;
+        }
+    }
+
+    private static Properties getProps() throws IOException {
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("database.properties"))){
+            props.load(in);
+            return props;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException("Config file not found", e);
+        }
+    }
+
+    //TODO: Обработка всех исключений, связанных с работой с базой данных должна находиться в dao
+    public static void close(Connection connection)
+    {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
